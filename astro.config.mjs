33e1d06d6bb0b8519059e4c197bb7087d2adeac7
Alohadancemeow <nawongsak@gmail.com>
2025-01-10 15:03:09 +0700

    Add Netlify adapter and update package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind()],
+  adapter: netlify(),
 });
